82a14f72d278a42aa86b1f53c41ea65e06d70ab3
keyur <13107634+kyurshah@users.noreply.github.com>
2024-11-27 19:18:34 +0530

    Completed first exercise - [personal cloned repo]

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let completeString = "";
+    for (let i = 0; i < num ; i++) {
+        completeString += string;
+    }
+    return completeString
 };
 
 // Do not edit below this line
